246c4842b381c9a453a8c8471595efc8e8d11a97
Marcos Neves <marcos.neves@gmail.com>
2014-03-21 10:13:15 -0300

    rails new usuarios -B

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Usuarios::Application.config.secret_key_base = 'b2b798ea7a2b074271da852c8217acd892de7d53799c5b28b51914cbea10110792526fe38d08ca88aa2237caa6fef6f22f21c467df28807687ec0aeb2698a4ae'
