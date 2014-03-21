4aa8057ae9ad0cc599c8ed5f44bd77e500a2ceb9
Marcos Neves <marcos.neves@gmail.com>
2014-03-21 10:25:01 -0300

    has_secure_password

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -69,6 +69,6 @@ class UsuariosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def usuario_params
-      params.require(:usuario).permit(:nome, :email, :password_digest)
+      params.require(:usuario).permit(:nome, :email, :password, :password_confirmation)
     end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,3 @@
 class Usuario < ActiveRecord::Base
+	has_secure_password
 end
